8d062010c31e45068572c9656a89d142f8086098
Atharva Sharma <106549203+Sharma-atharva@users.noreply.github.com>
2023-02-20 23:56:40 +0530

    Time: 10 ms (22.52%), Space: 39 MB (58.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int fib(int n) {
         //int start =0;
         //int ft=1;
-        if(n<2){
+        if(n==1||n==0){
             return n;
         }
         return fib(n-1) +fib(n-2);
