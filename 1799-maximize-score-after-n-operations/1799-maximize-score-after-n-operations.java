e4b55d0e908a025791837e44e5bac1b6979f75e8
Atharva Sharma <106549203+Sharma-atharva@users.noreply.github.com>
2023-05-14 22:49:33 +0530

    Time: 194 ms (62.50%), Space: 43.4 MB (15.28%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
@@ -0,0 +1,31 @@
+class Solution {
+    int[][] dp;
+    public int maxScore(int[] nums) {
+        int len = nums.length;
+        dp = new int[len/2][1<<len];
+        return helper(0, 0, nums);
+    }
+    public int helper(int cnt, int mask, int[] nums){
+        int len = nums.length;
+        if(cnt == len/2) return 0;
+        if(dp[cnt][mask]!=0) return dp[cnt][mask];
+        int res = 0;
+        for(int i=0; i<len-1; i++){
+            int next = mask|(1<<i);
+            if(mask==next) continue;
+            for(int j= i+1; j<len; j++){
+                int orig = next;
+                next |= (1<<j);
+                if(next == orig) continue;
+                int local = (cnt+1)*gcd(nums[i], nums[j]);
+                res = Math.max(res, local+helper(cnt+1, next, nums));
+                next = orig;
+            }
+        }
+        return dp[cnt][mask] = res;
+    }
+    public int gcd(int i, int j){
+        if(i==0) return j;
+        return gcd(j%i, i);
+    }
+}
